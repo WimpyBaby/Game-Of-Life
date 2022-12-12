b1a8e6f9be23d19b4eef8a2cb436a88f75b95321
WimpyBaby <83958291+WimpyBaby@users.noreply.github.com>
2022-12-12 01:05:07 +0100

    Add files via upload

--- antalgrannar.m
+++ antalgrannar.m
@@ -1,14 +1,10 @@
-%Calcualtes number of neighboring cells in a 3x3 block around cell of given
-%matrix A, in index A(r,c)
-
-
 function antalMatris = antalgrannar(y)
 [N,M]=size(y);
 
 antalMatris = zeros(N,M);
   for r=2:N-1
         for c=2:M-1
-    antalMatris(r,c)=sum(y(r-1:r+1,c-1:c+1),'all')-y(r,c); %the sum of the 3x3 block is equal to the number of living cells surrounding the checked cell
+    antalMatris(r,c)=sum(y(r-1:r+1,c-1:c+1),'all')-y(r,c);
         end
   end
  
--- levnadsregler.m
+++ levnadsregler.m
@@ -1,16 +1,9 @@
-
-
-
- %Uses currently set survival conditions and decides wether the currently
- %checked cell will survive, die, or spawn a new living cell
-
-
 function x = levnadsregler(antalMatris,y)
 [N,M] = size(antalMatris);
 
  for r=2:N-1
         for c=2:M-1
-  if(antalMatris(r,c)<2) %Most likely to be first in big empty spaces, like the puffer train simulation but order doesn't seem to affect processesing speed very much
+  if(antalMatris(r,c)<2)
         antalMatris(r,c)=0;
     elseif(antalMatris(r,c)==3)
         antalMatris(r,c)=1;
